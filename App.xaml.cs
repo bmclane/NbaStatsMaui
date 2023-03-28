35f91a4040da4828f5f26b33352afd8f3d17565e
bmclane <bmclane15@gmail.com>
2023-03-28 10:23:16 -0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NbaStatsMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NbaStatsMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
