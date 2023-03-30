afc29ee092a06b982ffb51dd688def24715c37f4
bmclane <bmclane15@gmail.com>
2023-03-30 16:04:08 -0400

    updated Teams page

---
+++ Helpers/Skeletons.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbaStatsMaui.Helpers
+{
+    public static class Skeletons
+    {
+        public static Team[] Teams = new[]
+        {
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team(),
+            new Team()
+        };
+
+    }
+}
--- ViewModels/TeamsViewModel.cs
+++ ViewModels/TeamsViewModel.cs
@@ -1,7 +1,29 @@
-﻿namespace NbaStatsMaui.ViewModels;
+﻿using Sharpnado.TaskLoaderView;
+
+namespace NbaStatsMaui.ViewModels;
 
 public partial class TeamsViewModel : BaseViewModel
 {
-    public TeamsViewModel(INbaApiService nbaApiService) : base(nbaApiService) { }
+    public TaskLoaderNotifier<List<Team>> Loader { get; }
+
+    public TeamsViewModel(INbaApiService nbaApiService) : base(nbaApiService)
+    {
+        Loader = new TaskLoaderNotifier<List<Team>>(LoadData);
+    }
+
+    public override Task InitializeAsync()
+    {
+        Loader.Load();
+
+        return base.InitializeAsync();
+
+    }
+
+    public async Task<List<Team>> LoadData(bool isRefresh)
+    {
+        var response = await NbaApiService.GetTeams();
+
+        return response.Data;
+    }
 
 }
